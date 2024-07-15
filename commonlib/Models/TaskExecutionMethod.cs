439e904104b9eacc6a16736143f59a932ea83a26
Awernet <andrew.zernov50@gmail.com>
2024-07-15 12:41:34 +0300

    Теперь Executes запускаются паралельно

--- commonlib/Models/TaskExecutionMethod.cs
+++ commonlib/Models/TaskExecutionMethod.cs
@@ -1,12 +1,17 @@
-﻿namespace commonlib.Models
+﻿using commonlib.Enums;
+
+namespace commonlib.Models
 {
     public readonly struct TaskExecutionMethod 
     {
         public TaskExecutionMethod(float sleep)
         {
             Delay = sleep;
+            Bits = new TaskBits();
+            Bits.SetBit(CustomTaskStatus.TS_RUNING);
         }
 
         public readonly float Delay;
+        public readonly TaskBits Bits;
     }
 }
