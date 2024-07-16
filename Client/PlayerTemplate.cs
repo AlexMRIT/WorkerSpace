6b0282e0a3cf6f31079d790531b22d483952a758
Awernet <andrew.zernov50@gmail.com>
2024-07-16 12:13:26 +0300

    Добавлена игра :D

---
+++ Client/PlayerTemplate.cs
@@ -0,0 +1,30 @@
+﻿using Client.Tasks;
+using commonlib.Interfaces;
+using commonlib;
+using commonlib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class PlayerTemplate
+    {
+
+        public PlayerTemplate(Drawer drawer) 
+        {
+            Drawer = drawer;
+
+            PlayerMovementAwaiter playerMovementAwaiter = new PlayerMovementAwaiter(this);
+            TaskBuilder taskBuilder = new TaskBuilder();
+            taskBuilder.AppendExecuteDelegateFunc(playerMovementAwaiter.Execute);
+
+            ITask task = CLIBTask.NewTask(taskBuilder);
+        }
+        public readonly Drawer Drawer;
+        public int x = 1;
+        public int y = 1;
+    }
+}
---
+++ Client/Tasks/PlayerMovementAwaiter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Tasks
+{
+    public class PlayerMovementAwaiter
+    {
+        public PlayerMovementAwaiter(PlayerTemplate player) 
+        {
+            _player = player;
+        }
+        private readonly PlayerTemplate _player;
+        public async Task Execute()
+        {
+            try
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (_player.Drawer.PaintElement(_player.x, _player.y - 1, "@"))
+                            _player.y--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (_player.Drawer.PaintElement(_player.x, _player.y + 1, "@"))
+                        _player.y++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (_player.Drawer.PaintElement(_player.x - 1, _player.y, "@"))
+                        _player.x--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (_player.Drawer.PaintElement(_player.x + 1, _player.y, "@"))
+                        _player.x++;
+                        break;
+                }
+
+                await Task.Delay(1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
+        }
+    }
+}
